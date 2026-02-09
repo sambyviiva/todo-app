fad1d8b89f9cd50248fb450203914b67e2da6f29
Sami Valkonen <sami.valkonen@kesko.fi>
2026-02-09 22:53:25 +0200

    Allow necessary cors method, add fastify logging

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,12 +8,12 @@ import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-
 require('dotenv').config();
 
 export const initApp = async (): Promise<FastifyInstance> => {
-  const server = fastify()
+  const server = fastify({ logger: true });
 
   server.setValidatorCompiler(validatorCompiler);
   server.setSerializerCompiler(serializerCompiler);
 
-  await server.register(cors, { origin: 'http://localhost:5173' })
+  await server.register(cors, { origin: 'http://localhost:5173', methods: ['GET', 'POST', 'PUT', 'DELETE'] });
 
   server.register(async (instance) => {
     instance.addHook("preHandler", getAuthMiddleware(process.env.API_KEY));
