b150a301d92b10a3b3aa066df3daf8798e566e83
Sami Valkonen <sami.valkonen@kesko.fi>
2026-02-10 21:20:43 +0200

    Fix: import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import './styles/App.css'
 import { Dashboard } from './components/TodoDashboard'
 import { useState } from 'react';
-import { CreatePage } from './components/CreatePage';
+import { CreatePage } from './components/CreateTodoPage';
 
 function App() {
   const [isCreatePageOpen, setIsCreatePageOpen] = useState<boolean>(false);
