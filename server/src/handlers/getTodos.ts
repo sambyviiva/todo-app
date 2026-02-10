bf6489d6a5ee2d5b9b0bf438f3a7b0a47a94865c
Sami Valkonen <sami.valkonen@kesko.fi>
2026-02-10 21:05:37 +0200

    do not return removed todos

--- server/src/handlers/getTodos.ts
+++ server/src/handlers/getTodos.ts
@@ -3,7 +3,7 @@ import db from "../db";
 import { GetTodosRoute, zTodosResponse } from "../schemas/todo";
 
 export const getTodos: RouteHandler<GetTodosRoute> = async (_request, reply) => {
-    const todosFromDb = await db.query('SELECT * FROM todo WHERE removed_at IS NULL');
+    const todosFromDb = await db.query("SELECT * FROM todo WHERE removed_at IS NULL AND status != 'REMOVED'");
     const validatedTodos = zTodosResponse.parse(todosFromDb.rows);
 
     return reply.send(validatedTodos);
