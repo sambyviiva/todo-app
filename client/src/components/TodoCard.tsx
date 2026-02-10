53f5c30a31039941c7266d3accf53c17356f3b1c
Sami Valkonen <sami.valkonen@kesko.fi>
2026-02-10 21:19:02 +0200

    Fix: long title or descriptions

--- client/src/components/TodoCard.tsx
+++ client/src/components/TodoCard.tsx
@@ -6,12 +6,11 @@ interface Props {
     removeTodoFn: (id: number) => Promise<void>,
 }
 export const TodoCard = ({ todo, updateTodoFn, removeTodoFn }: Props) => {
-
     return (
         <div className="rounded border-black border-4 p-4 mb-4 bg-indigo-950 w-100 relative">
-            <button className="absolute top-4 right-4 p-1 text-sm italic" onClick={() => removeTodoFn(todo.id)}>🗑</button>
-            <div className="text-2xl text-center mb-1 font-bold">{todo.title}</div>
-            <div className="h-30">{todo.description}</div>
+            <button className="absolute top-3 right-4 p-1 text-sm italic" onClick={() => removeTodoFn(todo.id)}>🗑</button>
+            <div className="text-2xl text-center mb-4 font-bold w-75 truncate">{todo.title}</div>
+            <div className="h-34 break-all">{todo.description}</div>
             <div className="relative h-10">
                 {(todo.status === "IN PROGRESS" || todo.status === "DONE") && <button className="absolute left-0" onClick={() => updateTodoFn(todo, "left")}>&#8592;</button>}
                 {(todo.status === "TODO" || todo.status === "IN PROGRESS") && <button className="absolute right-0" onClick={() => updateTodoFn(todo, "right")}>&#8594;</button>}
